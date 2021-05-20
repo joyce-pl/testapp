454ac3815eca23708e0573c55f09895f2e34535f
joyce-pl <karolinapolanska.gs@gmail.com>
2021-05-21 00:51:18 +0200

    pull request modifications

--- script.js
+++ script.js
@@ -1,76 +1,78 @@
-const button = document.getElementById('add');
-const list = document.getElementById('list');
-const textArea = document.getElementById('new-task');
+(() => {
+  function init() {
+    const list = document.getElementById('list');
+    const textArea = document.getElementById('new-task');
+    const button = document.getElementById('add');
+    const todos = [{ id: 1, name: "to do #1", checked: false },
+    { id: 2, name: "to do #2", checked: false },
+    { id: 3, name: "to do #3", checked: false }];
+  }
 
-generateId = () => {
-  return Date.now();
-}
+  init();
 
-changeTaskState = (e) => {
-  if(e.target.tagName === "LI") {
-    const taskId = e.target.getAttribute('data-id');
-    const task = todo.find(x => x.id == taskId);
-    if(task.checked === false) {
+  const generateId = () => Date.now();
+
+  const change = (liElement, inputElement, task) => {
+    if (task.checked === false) {
       task.checked = true;
-      e.target.querySelector('input').checked = true;
-      e.target.classList.add('done');  
+      inputElement.checked = true;
+      liElement.classList.add('done');
     } else {
       task.checked = false;
-      e.target.querySelector('input').checked = false;
-      e.target.classList.remove('done');
+      inputElement.checked = false;
+      liElement.classList.remove('done');
     }
   }
-  if(e.target.tagName === "INPUT") {
-    const taskId = e.target.parentElement.getAttribute('data-id');
-    const task = todo.find(x => x.id == taskId);
-    if(task.checked === false) {
-      task.checked = true;
-      e.target.checked = true;
-      e.target.parentElement.classList.add('done');
-    } else {
-      task.checked = false;
-      e.target.checked = false;
-      e.target.parentElement.classList.remove('done');
+
+  const findTask = (id) => todos.find(x => x.id == id);
+
+  changeTaskState = (e) => {
+    if (e.target.tagName === "LI") {
+      const taskId = e.target.getAttribute('data-id');
+      const task = findTask(taskId);
+      change(e.target, e.target.querySelector('input'), task);
     }
+    if (e.target.tagName === "INPUT") {
+      const taskId = e.target.parentElement.getAttribute('data-id');
+      const task = findTask(taskId);
+      console.log(e.target.checked);
+      change(e.target.parentElement, e.target, task);
+    }
+  };
+
+  const createLiElement = (newElement) => {
+    const li = document.createElement("li");
+    const box = document.createElement("input");
+    box.setAttribute('type', 'checkbox');
+    li.appendChild(box);
+    li.setAttribute('data-id', newElement.id);
+    li.appendChild(document.createTextNode(newElement.name));
+    li.addEventListener('click', changeTaskState);
+    list.appendChild(li);
   }
-};
 
-const todo = [{id: 1, name: "to do #1", checked: false}, 
-             {id: 2, name: "to do #2", checked: false}, 
-             {id: 3, name: "to do #3", checked: false}];
+  const displayToDoList = () => {
+    todos.forEach(todo => {
+      createLiElement(todo);
+    });
+  }
+  displayToDoList();
 
-const createLiElement = (newElement) => {
-  const li = document.createElement("li");
-  const box = document.createElement("input");
-  box.setAttribute('type', 'checkbox');
-  li.appendChild(box);
-  li.setAttribute('data-id', newElement.id);
-  li.appendChild(document.createTextNode(newElement.name));
-  li.addEventListener('click', changeTaskState);
-  list.appendChild(li);
-}
+  const addElementToList = () => {
+    const text = textArea.value.trim();
+    if (text.length > 0) {
+      const newElement = { id: generateId(), name: text, checked: false };
+      createLiElement(newElement);
+      todos.push(newElement);
+    }
+    textArea.value = "";
+  }
 
-const displayToDoList = () => {
-  todo.forEach(element => {
-    createLiElement(element);
+  button.addEventListener("click", () => addElementToList());
+  textArea.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      addElementToList();
+    }
   });
-}
-displayToDoList();
 
-const addElementToList = () => {
-  const text = textArea.value.trim();
-  if(text.length > 0) {
-    const newElement = {id: generateId(), name: text, checked: false};
-    createLiElement(newElement);
-    todo.push(newElement);
-  }
-  textArea.value = "";
-}
-
-button.addEventListener("click", () => addElementToList());
-textArea.addEventListener('keydown', (event) => {
-   if (event.key === 'Enter') { 
-    addElementToList();
-  }
-});
-
+})()
